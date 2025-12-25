3cc4ca6c5c813ca67685df754c49f752c1cc3c90
Nick Ooms <oomsni@gmail.com>
2025-12-25 17:16:57 +0100

    fix: update fetch URL to relative path for boxes API

--- src/util/fetchBoxes.ts
+++ src/util/fetchBoxes.ts
@@ -1,7 +1,7 @@
 import type { Box } from '../types/Box/Box';
 
 export const fetchBoxes = async (): Promise<Box[]> => {
-  const res = await fetch('http://localhost:3000/api/boxes');
+  const res = await fetch('../api/boxes');
   if (!res.ok) throw new Error('Failed to fetch boxes');
   return res.json();
 };
