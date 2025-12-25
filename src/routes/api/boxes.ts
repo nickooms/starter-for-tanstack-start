f04674c874a97fd3f8797db2153aac1c4884dc89
Nick Ooms <oomsni@gmail.com>
2025-12-25 23:08:07 +0100

    fix: update file path handling for boxes API to use absolute path

--- src/routes/api/boxes.ts
+++ src/routes/api/boxes.ts
@@ -1,12 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router';
 import fs from 'node:fs';
+import process from 'node:process';
+import path from 'node:path';
 // import type { Item } from '../../types/Box/Box';
 
 export const Route = createFileRoute('/api/boxes')({
   server: {
     handlers: {
       GET: async () => {
-        const boxes = await fs.promises.readFile('./data/boxes.json', 'utf-8').then(JSON.parse);
+        console.log('Current working directory:', process.cwd());
+        const filePath = path.join(process.cwd(), 'data', 'boxes.json');
+        const boxes = await fs.promises.readFile(filePath, 'utf-8').then(JSON.parse);
         return new Response(JSON.stringify(boxes), {
           headers: {
             'Content-Type': 'application/json',
